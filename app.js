843f12da7a5b909333b2be4c5563345157c0df44
Sunder Rawat <sunderrawat777@gmail.com>
2022-01-07 17:34:16 +0530

    user signup created sucess

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const express = require("express");
+const express = require('express');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+//for body parsing data
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
-    message: "success",
+    message: 'success',
     data: {
-      text: "hello from nodejs ",
+      text: 'hello from nodejs ',
     },
   });
 });
 
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'this route is not created',
+  });
+});
 
 module.exports = app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,43 @@ const User = require('./../model/userModel');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-  res.status(201).json({
-    message: 'user created successfully',
-  });
+router.post('/signup', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+      role: req.body.role || 'employee',
+    });
+    console.log(req.body);
+    console.log(user);
+    if (!user) {
+      res.status(400).json({
+        status: 'fail',
+        message: 'user not created',
+      });
+    }
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user,
+      },
+      message: 'user created successfully',
+    });
+  } catch (err) {
+      console.log(err);
+    res.status(400).json({
+      status: 'fail',
+      message: 'user not created',
+    });
+  }
 });
 
+router.post('/login', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'user login sccess',
+  });
+});
 module.exports = router;
