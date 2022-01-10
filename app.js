2575c1bd467defc114b816bec515d60f2853fb77
Sunder Rawat <sunderrawat777@gmail.com>
2022-01-10 12:23:56 +0530

    serve frontend to prod from build folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routes/userRoutes');
@@ -9,24 +10,27 @@ app.use(cors());
 //for body parsing data
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'success',
-    data: {
-      text: 'hello from nodejs ',
-    },
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'success',
+//     data: {
+//       text: 'hello from nodejs ',
+//     },
+//   });
+// });
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1', authController.protect);
 app.use('/api/v1/projects', authController.protect, projectRouter);
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: 'this route is not created',
-  });
-});
+// app.all('*', (req, res) => {
+//   res.status(404).json({
+//     status: 'fail',
+//     message: 'this route is not created',
+//   });
+// });
+
+//serve frontend
+app.use(express.static(__dirname + '/frontend/build'));
 
 module.exports = app;
--- frontend/src/apiUrl.js
+++ frontend/src/apiUrl.js
@@ -1,2 +1,5 @@
-const apiUrl = 'http://127.0.0.1:8000/api/v1';
+//for dev
+// const apiUrl = 'http://127.0.0.1:8000/api/v1';
+//for prod
+const apiUrl = '/api/v1';
 export default apiUrl;
